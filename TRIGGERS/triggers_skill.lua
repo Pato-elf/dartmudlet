35a931bc125aec06327b7f34140d626366faee40
Pato-elf <info@xlstech.net>
2024-10-02 15:24:58 -0400

    Update triggers_skill.lua

--- TRIGGERS/triggers_skill.lua
+++ TRIGGERS/triggers_skill.lua
@@ -1,60 +1,70 @@
 local Skill_Triggers = {}
-
 local triggers = {}
 
+
+
 local function setup(args)
-  local tempTriggers = {}
+	local tempTriggers = {}
 
-  --Skill Improvement for self
-  tempTriggers.SkillImproveSelf =
-  tempRegexTrigger("^(?:> )*\\* You think your ([\\w'\\-# ]+) skill has improved\\. \\*$"
-   ,[[
-      local name = Status.name
-      local skill_name = matches[2]
-      arguments = {name = name, skill_name = skill_name}
+	--trigger for skill improves - self
+	tempTriggers.SkillImproveSelf =
+	tempRegexTrigger("^(?:> )*\\* You think your ([\\w'\\-# ]+) skill has improved\\. \\*$",
+	[[
+		local name = Status.name
+		local skill_name = matches[2]
+		arguments = {name = name, skill_name = skill_name}
 
-      Events.raiseEvent("skillImproveEvent", arguments)
-    ]])
+		Events.raiseEvent("skillImproveEvent", arguments)
+	]])
 
-  --Skill Improvement for others
-  tempTriggers.SkillImproveOther =
-  tempRegexTrigger("^(?:> )*\\* You think that ([A-Za-z]+)'s ([\\w'\\-# ]+) skill has improved\\. \\*$"
-   ,[[
-      local name = matches[2]
-      local skill_name = matches[3]
-      arguments = {name = name, skill_name = skill_name}
 
-      Events.raiseEvent("skillImproveEvent", arguments)
-    ]])
 
-  --Skill Improvement mistake
-  tempTriggers.SkillMistake =
-  tempRegexTrigger("^(?:> )*\\(But you were mistaken.\\)"
-   ,[[
-      arguments = {}
+	--trigger for skill improves - others
+	tempTriggers.SkillImproveOther =
+	tempRegexTrigger("^(?:> )*\\* You think that ([A-Za-z]+)'s ([\\w'\\-# ]+) skill has improved\\. \\*$",
+	[[
+		local name = matches[2]
+		local skill_name = matches[3]
+		arguments = {name = name, skill_name = skill_name}
 
-      Events.raiseEvent("skillMistakeEvent", arguments)
-    ]])
+		Events.raiseEvent("skillImproveEvent", arguments)
+	]])
 
-    triggers = tempTriggers
+
+
+	--trigger for skill mistakes
+	tempTriggers.SkillMistake =
+	tempRegexTrigger("^(?:> )*\\(But you were mistaken.\\)",
+	[[
+		arguments = {}
+		Events.raiseEvent("skillMistakeEvent", arguments)
+	]])
+
+	triggers = tempTriggers
 end
+
+
 
 local function unsetup(args)
-  for i,v in pairs(triggers) do
-    killTrigger(v)
-  end
-  triggers = {}
+	for i,v in pairs(triggers) do
+		killTrigger(v)
+	end
+	triggers = {}
 end
+
+
 
 local function resetup(args)
-  unsetup(args)
-  setup(args)
+	unsetup(args)
+	setup(args)
 end
 
+
+
 Skill_Triggers = {
-  setup = setup
-  ,unsetup = unsetup
-  ,resetup = resetup
+	setup = setup,
+	unsetup = unsetup,
+	resetup = resetup
 }
 
 return Skill_Triggers

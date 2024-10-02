b0b94f3eb040332893ad835b76ebaa5ef737cd05
Pato-elf <info@xlstech.net>
2024-10-02 15:24:27 -0400

    Update aliases_skill.lua

--- ALIASES/aliases_skill.lua
+++ ALIASES/aliases_skill.lua
@@ -1,50 +1,72 @@
 local Skill = {}
 local aliases = {}
 
+
+
 local function setup(args)
-  tempAliases = {}
-  --Add update skill Alias
-  	tempAliases.updateSkill =
-      tempAlias('^\\/update ([\\w]+) ([A-Za-z_\' ]+) ([\\d]+)',
-								[[
-									local who = matches[2]
-                  local skill_name = matches[3]
-                  local count = matches[4]
-                  local arguments = {skill_name = skill_name, who = who, count = count}
-									Events.raiseEvent('updateSkillEvent', arguments)
-								]])
+	tempAliases = {}
+
+	--alias to manually insert skill into the database
+	tempAliases.insertSkill =
+	tempAlias('^\\/insert ([\\w]+) ([A-Za-z_\' ]+) ([\\d]+)',
+		[[
+			local who = matches[2]
+			local skill_name = matches[3]
+			local count = matches[4]
+			local arguments = {skill_name = skill_name, who = who, count = count}
+			Events.raiseEvent('insertSkillEvent', arguments)
+		]])
 
 
-  --Add Skill info Alias
-  	tempAliases.SkillInfo =
-      tempAlias('^\\/info(?: )?(.*)',
-								[[
-									local skill_name = matches[2]
-                  local arguments = {skill_name = skill_name, who = Status.name}
-									Events.raiseEvent('skillInfoEvent', arguments)
-								]])
 
-    aliases = tempAliases
+	--alias to update an existing skill
+	tempAliases.updateSkill =
+	tempAlias('^\\/update ([\\w]+) ([A-Za-z_\' ]+) ([\\d]+)',
+		[[
+			local who = matches[2]
+			local skill_name = matches[3]
+			local count = matches[4]
+			local arguments = {skill_name = skill_name, who = who, count = count}
+			Events.raiseEvent('updateSkillEvent', arguments)
+		]])
 
-  --Add announcer alias
+
+
+	--alias for skill information
+	tempAliases.SkillInfo =
+	tempAlias('^\\/info(?: )?(.*)',
+		[[
+			local skill_name = matches[2]
+			local arguments = {skill_name = skill_name, who = Status.name}
+			Events.raiseEvent('skillInfoEvent', arguments)
+		]])
+
+	aliases = tempAliases
+
 end
+
+
 
 local function unsetup(args)
-  for i,v in pairs(aliases) do
-    killAlias(v)
-  end
+	for i,v in pairs(aliases) do
+		killAlias(v)
+	end
 end
 
+
+
 local function resetup(args)
-  unsetup(args)
-  setup(args)
+	unsetup(args)
+	setup(args)
 end
+
+
 
 Skill =
 {
-  setup = setup
-  ,unsetup = unsetup
-  ,resetup = resetup
+	setup = setup,
+	unsetup = unsetup,
+	resetup = resetup
 }
 
 return Skill
